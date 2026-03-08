26fcb65fb0a9b99b6bb28d2fb19955b0cf0504b7
Michel Ferré Díaz <75095570+EntropicPhys@users.noreply.github.com>
2026-03-08 16:04:40 +0200

    Add sG function for semi-analytic calculations

---
+++ semi_analytic/sG.m
@@ -0,0 +1,5 @@
+function r=sG(p,u)
+nu=p.nu;par=u(nu+1:end);dw=par(11);dh=par(12);s=par(13);
+K=p.mat.K;Kx=p.mat.Kx;M=p.mat.M;
+bK=kron([[1,0,0];[0,dw,0];[0,0,dh]],K);sKx=kron([[s,0,0];[0,s,0];[0,0,s]],Kx);
+r=(bK-sKx)*u(1:nu)-M*nodalf(p,u);
