7dc4726002e9948a5ee940059c8ec57315f10275
Michel Ferré Díaz <75095570+EntropicPhys@users.noreply.github.com>
2026-03-08 16:01:56 +0200

    Delete semi_analytic_TI directory

--- semi_analytic_TI/oosetfemops.m
+++
@@ -1,5 +0,0 @@
-function p=oosetfemops(p)
-[K,M,~]=p.pdeo.fem.assema(p.pdeo.grid,1,1,1);p.mat.K=K;
-p.mat.M=kron([[1,0,0];[0,1,0];[0,0,1]],M);
-Kx=convection(p.pdeo.fem,p.pdeo.grid,1);
-p.mat.Kx=Kx;
