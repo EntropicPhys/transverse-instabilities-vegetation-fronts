d755367ee488d16ebc66637b30617bbae6151b2c
Michel Ferré Díaz <75095570+EntropicPhys@users.noreply.github.com>
2026-09-13 18:03:39 +0300

    Update sG.m

--- bif_diag/sG.m
+++ bif_diag/sG.m
@@ -1,4 +1,4 @@
-function r=sG(p,u)
+function r=sG(p,u) % PDE discrete approximation
 par=u(p.nu+1:end); dw=par(14);dh=par(15); 
 K=p.mat.K; M=p.mat.M; bK=kron([[1,0,0];[0,dw,0];[0,0,dh]],K);
-r=bK*u(1:p.nu) - M*nodalf(p,u); 
+r=bK*u(1:p.nu) - M*nodalf(p,u); % PDE discrte approximation assembly 
