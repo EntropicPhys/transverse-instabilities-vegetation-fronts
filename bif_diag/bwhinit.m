61601b0ca5d2b345d863b0f45b8df89e189c647a
Michel Ferré Díaz <75095570+EntropicPhys@users.noreply.github.com>
2026-09-13 18:22:17 +0300

    Update bwhinit.m

--- bif_diag/bwhinit.m
+++ bif_diag/bwhinit.m
@@ -1,10 +1,14 @@
-function p=bwhinit(p,lx,nx,par,b0,w0,h0,dir)
-p=stanparam(p);if ~exist(['./' dir],'dir'); mkdir(['./' dir]); end
-p=setfn(p,dir); screenlayout(p);
-p.nc.neq=3; p.sw.sfem=-1; p.fuha.sG=@sG; p.fuha.sGjac=@sGjac; p.fuha.e2rs=@e2rs_ref;
-p.fuha.outfu=@sgbra; pde=stanpdeo1D(lx,2*lx/nx); p.pdeo=pde; p.vol=2*lx;
+function p=bwhinit(p,lx,nx,par,b0,w0,h0,dir) % Initialization of continuation and FEM operators/space based on pdetoolbox  
+p=stanparam(p); % sets main continuation parameter and functions 
+p=setfn(p,dir);  % Creates dir as directory.
+p.fuha.outfu=@sgbra; % sgbra.m is set as file to compute continuation outputs.
+
+% PDE objects and key quantities are defined; see stanparam for details.
+pde=stanpdeo1D(lx,2*lx/nx); p.pdeo=pde; p.vol=2*lx;
 p.np=pde.grid.nPoints; p.nu=p.nc.neq*p.np; p.sol.xi=1/(p.nu);
 p=setfemops(p); p.nc.ilam=1; p.nc.lammin=0;  
-b=b0*ones(p.np,1); w=w0*ones(p.np,1); h=h0*ones(p.np,1); p.u=[b;w;h;par];
-p.sol.ds=0.01; p.sw.verb=2;
-p.plot.bpcmp=2; p.plot.cl={'black','blue','red'}; plotsol(p);
+p.nc.neq=3; p.sw.sfem=-1; 
+
+b=b0*ones(p.np,1); w=w0*ones(p.np,1); h=h0*ones(p.np,1); p.u=[b;w;h;par]; % initial conditions and assembly of p.u
+p.sol.ds=0.01; p.sw.verb=2; % initial arc-length step size and terminal print protocol
+screenlayout(p); p.plot.bpcmp=2; p.plot.cl={'black','blue','red'}; plotsol(p); % generic plot parameters; see staparam for details.
