a004134df59906dc016c3a36e28f0c8fa51b249b
Michel Ferré Díaz <75095570+EntropicPhys@users.noreply.github.com>
2026-09-13 17:59:25 +0300

    Update sgbra.m

--- bif_diag/sgbra.m
+++ bif_diag/sgbra.m
@@ -1,4 +1,6 @@
-function out=sgbra(p,u)
+function out=sgbra(p,u) 
+%sgbra defines and calculates the continuation output. 
+% Here we calcualte L1-norm(b), L2-norm(b), max(b) and min(b) 
 par=p.u(p.nu+1:end); u=u(1:p.nu); n=p.np; M=p.mat.M(1:n,1:n); 
 l1v=sum(M*u(1:n))/p.vol; l2v=sqrt((u(1:n)'*(M*u(1:n)))/p.vol); 
 out=[l1v; l2v; max(abs(u(1:n))); min(abs(u(1:n))); par]; 
